4f139ad0522ad9946d299ce8f68ef957fdee9a6e
Vishal Apte <vishalapte@gmail.com>
2022-02-22 09:17:03 +0530

    Fix broken merge

--- shell/venv_plus.sh
+++ shell/venv_plus.sh
@@ -1,5 +1,4 @@
 dj () {
-<<<<<<< HEAD
     cd $HOME/dev/PROJECT
     if [[ $1 == "" ]]; then
         python manage.py help --settings=project.settings.dev
@@ -7,16 +6,3 @@ dj () {
         python manage.py $1 ${@:2} --settings=project.settings.dev
     fi
 }
-=======
-    cd PROJDIR
-    if [[ $1 == "" ]]; then
-        python manage.py help --settings=PROJECT.settings.dev
-    else
-        python manage.py $1 ${@:2} --settings=PROJECT.settings.dev
-    fi
-}
-
-runserver () {
-    dj runserver
-}
->>>>>>> 91f222d (update: venv)
